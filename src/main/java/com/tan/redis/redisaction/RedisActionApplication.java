27a2d0b2b915edd00edf3df7370017f591092586
Unknown <15218835680@163.com>
2018-07-31 16:13:06 +0800

    redis in action

---
+++ src/main/java/com/tan/redis/redisaction/RedisActionApplication.java
@@ -0,0 +1,12 @@
+package com.tan.redis.redisaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisActionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tan/redis/redisaction/RedisActionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tan.redis.redisaction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisActionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
